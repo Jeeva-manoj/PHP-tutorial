89d91b2663a19dc6b068c15d4d72128ccc2418f7
Jeeva Manoj <63700108+Jeeva-manoj@users.noreply.github.com>
2021-12-02 15:40:43 +0530

    Add files via upload

---
+++ arrays.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php 
+        
+        $array = [1,2,3,4,5,6];  //regular array
+
+        echo $array[0] . "<br>";
+
+        $assoc_array = ["name1"=>"Hello", "name2"=>"World"];   //associative array
+
+        echo $assoc_array["name1"];
+        
+        
+        ?>
+    </body>
+</html>
---
+++ variables_math.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+        
+        $name = "Hello";     //variables
+        $number = 10;
+        
+        echo $name . "<br>";
+        echo $number . "<br>";
+
+        $num1 = 10;
+        $num2 = 20;
+        $num3 = 30;
+
+        echo $num1 + $num2 . "<br>";   //math operations
+     
+        echo $num1 * $num2 / $num3;        
+        
+        ?>
+    </body>
+</html>
